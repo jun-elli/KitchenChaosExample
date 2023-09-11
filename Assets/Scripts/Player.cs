d81ee6c66f8b544753d6fe44dc47a63e6216a38e
Jun <jun279r@gmail.com>
2023-09-11 18:57:06 +0200

    Create player character and add movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speedMultiplier = 7f;
+    private void Update()
+    {
+        ControllMovement();
+    }
+
+    private void ControllMovement()
+    {
+        Vector2 input = new(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            input.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            input.y -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input.x += 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            input.x -= 1f;
+        }
+
+        input = input.normalized;
+        // Add vector to actual position with speed
+        Vector3 moveDirection = new(input.x, 0f, input.y);
+        transform.position += moveDirection * speedMultiplier * Time.deltaTime;
+
+    }
+}
