94e08ecd8cb0dc2a55d65078ca4a051e7cc79b36
Jun <jun279r@gmail.com>
2023-09-18 15:24:48 +0200

    Add collision detection for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,37 +8,72 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speedMultiplier = 7f;
     private float _rotationSpeed = 10f;
     public bool IsWalking { get; private set; }
-
+    private const float PLAYER_HEIGHT = 2f;
+    private const float PLAYER_RADIUS = 0.7f;
 
     private void Update()
     {
-        ControllMovement();
+        ControlMovement();
     }
 
-    private void ControllMovement()
+    private void ControlMovement()
     {
-
-        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
-
         // Player is Idle
         IsWalking = false;
 
+        // Get input and vector to move
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        Vector3 moveDirection = new(inputVector.x, 0f, inputVector.y);
+
+        // Check if we can move in that direction
+        Vector3 bottomPoint = transform.position;
+        Vector3 topPoint = transform.position + Vector3.up * PLAYER_HEIGHT;
+        // Max distance it wants to move
+        float moveDistance = _speedMultiplier * Time.deltaTime;
+
+        bool canMove = !Physics.CapsuleCast(bottomPoint, topPoint, PLAYER_RADIUS, moveDirection, moveDistance);
+        bool wantsToMove = inputVector != Vector2.zero;
+
+        if (!canMove)
+        {
+            // Check if we can move on the x
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            bool canMoveX = !Physics.CapsuleCast(bottomPoint, topPoint, PLAYER_RADIUS, moveDirectionX, moveDistance);
+
+            if (canMoveX)
+            {
+                moveDirection = moveDirectionX;
+                canMove = true;
+            }
+            else
+            {
+                // Check if we can move on the z
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                bool canMoveZ = !Physics.CapsuleCast(bottomPoint, topPoint, PLAYER_RADIUS, moveDirectionZ, moveDistance);
+
+                if (canMoveZ)
+                {
+                    moveDirection = moveDirectionZ;
+                    canMove = true;
+                }
+            }
+        }
+
         // If there's movement:
-        if (inputVector != Vector2.zero)
+        if (wantsToMove && canMove)
         {
             // Player is walking
             IsWalking = true;
 
-
             // Add vector to actual position with speed
-            Vector3 moveDirection = new(inputVector.x, 0f, inputVector.y);
-            transform.position += moveDirection * _speedMultiplier * Time.deltaTime;
+            transform.position += moveDirection * moveDistance;
 
             // Add rotation to make z point to our moving direction
             transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * _rotationSpeed);
         }
 
     }
+
 
 
 }
