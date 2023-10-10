b4b66252abac4fc82c59cd7f9373f74e2b0cc056
Jun <jun279r@gmail.com>
2023-10-10 16:23:27 +0200

    Add scenes and loading

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        MainTitleScene,
+        GameScene,
+        LoadingScene
+    }
+    public static Scene targetScene;
+
+    public static void Load(Scene scene)
+    {
+        targetScene = scene;
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallback()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
